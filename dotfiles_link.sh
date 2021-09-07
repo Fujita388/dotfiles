639907409f793d69e01809f442ec52eb598cb34c
N.Fujita <naofuji.1220@gmail.com>
2021-09-07 17:57:48 +0900

    initial commit

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -4,7 +4,7 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/robbyrussell.zsh-theme ~/.oh-my-zsh/themes/robbyrussell.zsh-theme
+ln -s ~/dotfiles/my_zsh_theme.zsh-theme ~/.oh-my-zsh/themes/my_zsh_theme.zsh-theme
 
 
 
