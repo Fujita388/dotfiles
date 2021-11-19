13bd21d9188e669eb99f08d2bf4c51feaff6ccd1
N.Fujita <naofuji.1220@gmail.com>
2021-11-20 03:58:11 +0900

    adds .gitconfig

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -4,8 +4,5 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/my_zsh_theme.zsh-theme ~/.oh-my-zsh/themes/my_zsh_theme.zsh-theme
-
-
-
-
